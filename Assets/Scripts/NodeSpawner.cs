ff37db9f88a3e17676bf34b55faf3859bd3e251c
CobaltBru <97279958+CobaltBru@users.noreply.github.com>
2025-01-07 18:02:38 +0900

    Make Project

---
+++ Assets/Scripts/NodeSpawner.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public enum FRUIT { Apple = 1, Banana, Grape, Kiwi, Orange, Pineapple }
+public class NodeSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] Fruitlist;
+
+
+    void SpawnBlock()
+    {
+
+    }
+}
