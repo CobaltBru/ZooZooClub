107cebc5867dcabdda3ff7cfd745f1ca0559a565
CobaltBru <97279958+CobaltBru@users.noreply.github.com>
2025-01-09 18:08:28 +0900

    NodeSpawner

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,8 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    Vector2Int?[] neighborNodes;
+
+}
--- Assets/Scripts/NodeSpawner.cs
+++ Assets/Scripts/NodeSpawner.cs
@@ -1,14 +1,47 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public enum FRUIT { Apple = 1, Banana, Grape, Kiwi, Orange, Pineapple }
 public class NodeSpawner : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] Fruitlist;
+    private GameObject nodePrefab;
+    [SerializeField]
+    private RectTransform nodeRect;
 
-
-    void SpawnBlock()
+    void SpawnNode(Vector2Int panelSize)
     {
+        List<Node> nodeList = new List<Node>(panelSize.x * panelSize.y);
 
+        for (int y = 0; y < panelSize.y; y++) 
+        {
+            for (int x = 0; x < panelSize.x; x++) 
+            {
+                GameObject clone = Instantiate(nodePrefab, nodeRect.transform);
+                Vector2Int point = new Vector2Int(x,y); // 积己等 Node狼 谅钎
+
+                Vector2Int?[] neighborNodes = new Vector2Int?[4]; //积己等 Node狼 林函谅钎
+
+                Vector2Int right = point + Vector2Int.right;
+                Vector2Int down = point + Vector2Int.down;
+                Vector2Int left = point + Vector2Int.left;
+                Vector2Int up = point + Vector2Int.up;
+
+                if (isValid(right, panelSize)) neighborNodes[0] = right;
+                if (isValid(down, panelSize)) neighborNodes[1] = down;
+                if (isValid(left, panelSize)) neighborNodes[2] = left;
+                if (isValid(up, panelSize)) neighborNodes[3] = up;
+            }
+        }
+        
+
+
+
+    }
+    bool isValid(Vector2Int point, Vector2Int panelSize)
+    {
+        if (point.x < 0 || point.x >= panelSize.x || point.y < 0 || point.y >= panelSize.y)
+            return false;
+        else return true;
     }
 }
